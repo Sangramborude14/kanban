394945b4e91707b494e3fb979ad8beca723769c3
Sangram <sangram2mail@gmail.com>
2026-05-25 10:57:48 +0530

    added AddTask and AddColumn function and button

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,12 +1,60 @@
 "use client"
 import { useState } from "react";
-import { kanban,Column} from "@/types/board"
-import { title } from "process";
+import { kanban,Column,Task} from "@/types/board";
+ 
 
 
 export default function Board(){
     const timestamp = Date.now();
     const date = new Date(timestamp);
+
+    const addColumn = () => {
+    const title = prompt("enter the column title");
+
+    if(!title) return;
+    const newColumn: Column = {
+        id: `col-${Date.now()}`,
+        title: title,
+        tasks: [],
+        boardId: board.id
+    }
+    setBoard(prevBoard => {
+        return {...prevBoard,
+            columns: [...prevBoard.columns,newColumn]
+        }
+    })
+    }
+
+    const addTask = (column: Column) => {
+    const id = `task-${Date.now()}`
+    const title = prompt("enter the task title");
+    const description = prompt("enter the description");
+    const columnId = column.id
+    const createdAt = date.toDateString();
+
+    if(!title) return;
+    if(columnId !== column.id) return;
+
+    const newTask:Task = {
+        id: id,
+        title: title,
+        description: description,
+        columnId: columnId,
+        createdAt: createdAt,
+    }
+    setBoard(prevBoard => {
+        return {...prevBoard,columns: (
+            prevBoard.columns.map((column) => {
+            if (column.id === columnId){
+                return { ...column,tasks: [...column.tasks,newTask]}
+            }
+            return column;
+        }))
+
+        }
+    })
+}
+
     const task1 = {
         id: '1',
         title: 'play gta 5',
@@ -55,32 +103,49 @@ export default function Board(){
     })   
     
     return(<>
-    <div className="h-screen overflow-hidden w-screen">
-        <h1 className={boardTitlecss}>
-            {board.title}
-        </h1>
-        <div className="m-4 flex w-full text-center">
-            {board.columns.map((column) => (
-                <div key={column.id} className="flex-1 mx-4 p-3 border-3 border-cyan-950 mr-12">
-                    <h1 className="text-2xl text-red-400">
-                        {column.title}
-                        </h1>
-                    <div>
-                        {column.tasks.map((task) => (
-                            <div key={task.id} className="my-2 bg-gray-900 py-6 px-4">
-                                <h1 className="font-bold mb-3 text-xl">
-                                    {task.title}
-                                    </h1>
-                                <p className="font-light text-sm italic text-gray-300">{task.description}</p>
-                                <span className="font-thin text-xs text-blue-400">created at: {task.createdAt}</span>
-                            </div>
-                    ))} 
-        </div> 
+  <div className="h-screen overflow-hidden w-screen">
+    <h1 className={boardTitle_css}>
+      {board.title}
+    </h1>
+    <div className="m-4 text-center flex justify-center">
+      {board.columns.map((column) => (
+        <div key={column.id} className="flex-1 mx-4 p-3 border-3 border-cyan-950 mr-12">
+          <h1 className="text-2xl text-red-400">
+            {column.title}
+          </h1>
+          <div>
+            {column.tasks.map((task) => (
+              <div key={task.id} className={taskDiv_css}>
+                <h1 className="font-bold mb-3 text-xl">
+                  {task.title}
+                </h1>
+                <p className={taskPara_css}>{task.description}</p>
+                <span className={taskSpan_css}>created at: {task.createdAt}</span>
+              </div>
+            ))}
+            <div className="">
+              <span>Add a Task</span>
+              <br />
+              <button className={addTaskBtn_css} onClick={() => addTask(column)} >+</button>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div className="flex-1 mx-4 p-3 border-3 border-cyan-950 mr-12">
+        <h1 className="text-2xl text-red-400">Add a column</h1>
+        <div className={taskDiv_css}>
+          <button onClick={addColumn} className="my-2 font-extrabold text-3xl p-3 px-5 border-4 rounded-full">
+            +
+          </button>
+        </div>
+      </div>
     </div>
-))}
-     </div>
-    </div>
-    </>)
+  </div>
+</>)
 }
 
-const boardTitlecss = "text-center font-bold text-5xl p-2 m-6"
+const boardTitle_css = "text-center font-bold text-5xl p-2 m-6"
+const taskPara_css = "font-light text-sm italic text-gray-300"
+const taskSpan_css = "font-thin text-xs text-blue-400"
+const taskDiv_css = "my-2 bg-gray-900 py-6 px-4"
+const addTaskBtn_css = "font-extrabold  px-4 py-0 rounded-full m-2 text-2xl bg-gray-800 text-red-600"
